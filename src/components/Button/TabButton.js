4f242941c21b129f0102122eb0d9e1495b4a9598
Vitor Py <vitorpy@gmail.com>
2020-07-23 14:43:21 +0100

    Removing hover from the tab bar

--- src/components/Button/TabButton.js
+++ src/components/Button/TabButton.js
@@ -17,20 +17,12 @@ const Button = styled.button`
 	border: none;
 	flex: 1;
 	font-size: 32px;
-	letter-spacing: 1px;
 	transition: all ease-in 0.1s;
 	font-family: 'archia-medium';
 	background-color: ${props => props.theme.colorStyles.background};
 	color: ${props => props.theme.colorStyles.subtext};
 	&:disabled {
 		opacity: 0.3;
-	}
-	&:hover:not(:disabled),
-	&:focus:not(:disabled) {
-		background-color: ${props => props.theme.colorStyles.borders};
-		border-bottom: 8px solid
-			${props =>
-				props.isSelected ? props.theme.colorStyles.accentDark : props.theme.colorStyles.borders};
 	}
 `;
 
